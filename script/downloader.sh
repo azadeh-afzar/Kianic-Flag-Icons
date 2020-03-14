86218c7e355127ad89c65eecf6959ed57b3fdc0e
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-03-14 11:39:58 +0330

    add: updater scripts

---
+++ script/downloader.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CURRDIR="$(echo $PWD)"
+ICONDIR="$(echo ${PWD%/*})/kiaflagcons/icons"
+TEMPDIR="temp"
+TEMPICON_4x3="$( echo "$TEMPDIR/"*"/flags/4x3" )"
+TEMPICON_1x1="$( echo "$TEMPDIR/"*"/flags/1x1" )"
+
+LOCATION=$(curl -s https://api.github.com/repos/lipis/flag-icon-css/releases/latest \
+| grep "tag_name" \
+| awk '{print "https://github.com/lipis/flag-icon-css/archive/" substr($2, 2, length($2)-3) ".tar.gz"}') \
+
+mkdir -p $TEMPDIR && curl -L $LOCATION | tar -xzv --directory $TEMPDIR
+
+rm -rf "$ICONDIR" && mkdir -p "$ICONDIR" && mkdir -p "$ICONDIR/4x3" && mkdir -p "$ICONDIR/1x1"
+
+mv -v  $TEMPICON_4x3/* "$ICONDIR/4x3" && mv -v  $TEMPICON_1x1/* "$ICONDIR/1x1"
+
+rm -rf $TEMPDIR
---
+++ script/updater.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# Python Standard Library
+import os
+import glob
+import subprocess
+
+# configure path variables.
+file_path = os.path.abspath(os.path.dirname(__file__))
+base_path = os.path.abspath(os.path.dirname(file_path))
+kiaflagcons_path = os.path.join(base_path, "kiaflagcons")
+icon_path = os.path.join(kiaflagcons_path, "icons", "4x3")
+scss_file_path = os.path.join(kiaflagcons_path, "sass", "list.scss")
+
+# download latest simple icon release.
+process =subprocess.run(["./downloader.sh"], stdout=subprocess.PIPE)
+
+# list name of all the icon svg files.
+file_names = [os.path.splitext(file)[0] for file in os.listdir(icon_path) if file.lower().endswith(".svg")]
+
+# sort file names alphabetically.
+file_names = sorted(file_names, key=str.lower) 
+
+# scss file template.
+
+scss_file_template = """// This an auto generated file, do not modify!
+@if variable-exists(include) {{
+  @each $country in $include {{
+    @include kiaflagcons($country);
+  }}
+}} @else {{
+{0}
+}}
+"""
+
+icon_include_template = "  @include kiaflagcons({0});"
+
+# generate new icon list.
+new_icon_list = [icon_include_template.format(file_name) for file_name in file_names]
+new_icon_list_compiled = "\n".join(new_icon_list)
+
+# insert new list into the file template.
+new_scss_file = scss_file_template.format(new_icon_list_compiled)
+
+# write new scss file to disk.
+with open(scss_file_path, "w+") as file:
+    file.write(new_scss_file)
+
