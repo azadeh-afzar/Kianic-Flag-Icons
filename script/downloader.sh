6527065cb508ee0e0e5a26fce132a4d924b0dbae
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-06-14 19:16:35 +0430

    modify: remove verbose flag

--- script/downloader.sh
+++ script/downloader.sh
@@ -10,10 +10,10 @@ LOCATION=$(curl -s https://api.github.com/repos/lipis/flag-icon-css/releases/lat
 | grep "tag_name" \
 | awk '{print "https://github.com/lipis/flag-icon-css/archive/" substr($2, 2, length($2)-3) ".tar.gz"}') \
 
-mkdir -p $TEMPDIR && curl -L "$LOCATION" | tar -xzv --directory $TEMPDIR
+mkdir -p $TEMPDIR && curl -L "$LOCATION" | tar -xz --directory $TEMPDIR
 
 rm -rf "$ICONDIR" && mkdir -p "$ICONDIR" && mkdir -p "$ICONDIR/4x3" && mkdir -p "$ICONDIR/1x1"
 
-mv -v  $TEMPICON_4x3/* "$ICONDIR/4x3" && mv -v  $TEMPICON_1x1/* "$ICONDIR/1x1"
+mv $TEMPICON_4x3/* "$ICONDIR/4x3" && mv $TEMPICON_1x1/* "$ICONDIR/1x1"
 
 rm -rf $TEMPDIR
