b13c5a96825c1ea73283b1b3cabf70d83e5efef9
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-06-15 18:01:53 +0430

    modify: don not pipe curl into tar

--- script/downloader.sh
+++ script/downloader.sh
@@ -2,18 +2,37 @@
 
 CURRDIR="$PWD"
 ICONDIR="${PWD%/*}/kiaflagcons/icons"
+ICON_TARGZ="kiaflagcons.tar.gz"
 TEMPDIR="temp"
 TEMPICON_4x3="$( echo "$TEMPDIR/"*"/flags/4x3" )"
 TEMPICON_1x1="$( echo "$TEMPDIR/"*"/flags/1x1" )"
 
+# Create temporary directory.
+mkdir -p $TEMPDIR
+
+# Reset current icon directories.
+rm -rf "$ICONDIR"
+mkdir -p "$ICONDIR"
+mkdir -p "$ICONDIR/4x3"
+mkdir -p "$ICONDIR/1x1"
+
+# Create download link.
 LOCATION=$(curl -s https://api.github.com/repos/lipis/flag-icon-css/releases/latest \
 | grep "tag_name" \
 | awk '{print "https://github.com/lipis/flag-icon-css/archive/" substr($2, 2, length($2)-3) ".tar.gz"}') \
 
-mkdir -p $TEMPDIR && curl -L "$LOCATION" | tar -xz --directory $TEMPDIR
+# Download.
+curl -L "$LOCATION" > "$ICON_TARGZ"
 
-rm -rf "$ICONDIR" && mkdir -p "$ICONDIR" && mkdir -p "$ICONDIR/4x3" && mkdir -p "$ICONDIR/1x1"
+# Extract
+tar -xz --file "$ICON_TARGZ" --directory "./$TEMPDIR"
 
-mv $TEMPICON_4x3/* "$ICONDIR/4x3" && mv $TEMPICON_1x1/* "$ICONDIR/1x1"
+# Delete downloaded tar.gz
+rm "$ICON_TARGZ"
 
+# Move files from temporary directory to main directory.
+mv $TEMPICON_4x3/* "$ICONDIR/4x3"
+mv $TEMPICON_1x1/* "$ICONDIR/1x1"
+
+# Delete temporary folder.
 rm -rf $TEMPDIR
